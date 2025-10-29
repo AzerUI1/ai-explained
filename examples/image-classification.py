6d3061c12febb350baa781bd50dc04aba7af1fc0
Azar Aslanov <azer0808aslanov@gmail.com>
2025-10-29 18:05:16 +0400

    Add dummy image classification script

---
+++ examples/image-classification.py
@@ -0,0 +1,9 @@
+# dummy image classification
+import random  
+
+images = ["cat.jpg", "dog.jpg", "car.jpg"] 
+labels = ["Cat", "Dog", "Car"]  
+
+for img in images:
+    predicted = random.choice(labels)  
+    print(f"Image {img} is classified as {predicted}")  
